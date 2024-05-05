05ec2e4dc8f84f5a26d96022260f658015f41d18
pctechkid <omboyronnel@gmail.com>
2024-05-05 23:47:34 +0800

    Update bard.js

--- modules/commands/bard.js
+++ modules/commands/bard.js
@@ -300,8 +300,99 @@
 // }
 
 
-const axios = require("axios");
-let fontEnabled = false;
+// const axios = require("axios");
+// let fontEnabled = false;
+
+// module.exports.config = {
+//   name: "bard",
+//   version: "1",
+//   usePrefix: true,
+//   hasPermission: 0,
+//   credits: "Aki Hayakawa",
+//   description: "Search using chatgpt",
+//   commandCategory: "ai",
+//   usages: "<ask>",
+//   cooldowns: 5,
+// };
+
+// async function convertImageToCaption(imageURL, api, event, inputText) {
+//   try {
+//     api.sendMessage("Generating response ✅", event.threadID, event.messageID);
+
+//     const response = await axios.get(`https://aki-gemini-pro-vision.vercel.app/gemini?prompt=${encodeURIComponent(inputText)}&url=${encodeURIComponent(imageURL)}`);
+//     const caption = response.data.response;
+
+//     if (caption) {
+//       const formattedCaption = formatFont(caption);
+//       api.sendMessage(`${formattedCaption}`, event.threadID, event.messageID);
+//     } else {
+//       api.sendMessage("Failed to recognize image.", event.threadID, event.messageID);
+//     }
+//   } catch (error) {
+//     console.error("Error while recoginizing image:", error);
+//     api.sendMessage("An error occured while recognizing the image.", event.threadID, event.messageID);
+//   }
+// }
+
+// module.exports.handleEvent = async function ({ api, event }) {
+//   if (!(event.body.toLowerCase().startsWith(`${global.config.PREFIX}ai`))) return;
+
+//   const args = event.body.split(/\s+/);
+//   args.shift();
+
+//   if (event.type === "message_reply") {
+//     if (event.messageReply.attachments[0]) {
+//       const attachment = event.messageReply.attachments[0];
+
+//        if (attachment.type === "photo") {
+//         const imageURL = attachment.url;
+//         convertImageToCaption(imageURL, api, event, args.join(' '));
+//         return;
+//       }
+//     }
+//   }
+
+//   const inputText = args.join(' ');
+
+//   if (!inputText && (!event.messageReply.attachments[0] || event.messageReply.attachments[0].type !== "photo")) {
+//     return api.sendMessage("Hello, I'm GPT-4 by Ronnel. How may I help you?", event.threadID, event.messageID);
+//   }
+
+//   if (args[0] === "on") {
+//     fontEnabled = true;
+//     api.sendMessage({ body: "Font Formatting Enabled" }, event.threadID, event.messageID);
+//     return;
+//   }
+
+//   if (args[0] === "off") {
+//     fontEnabled = false;
+//     api.sendMessage({ body: "Font Formatting Disabled" }, event.threadID, event.messageID);
+//     return;
+//   }
+
+//   api.sendMessage("Generating response ✅", event.threadID, event.messageID);
+
+//   try {
+//     var uid = event.senderID;
+//     const response = await axios.get(`https://deku-rest-api.replit.app/gpt4?prompt=${encodeURIComponent(inputText)}&uid=${uid}`);
+//     if (response.status === 200 && response.data.gpt4) {
+//         let formattedResponse = formatFont(response.data.gpt4);
+//         formattedResponse = formattedResponse.replace(/\n\[Image of .*?\]|(\*\*)/g, '').replace(/^\*/gm, '•');
+//         api.sendMessage(`${formattedResponse}`, event.threadID, event.messageID);
+//     } else {
+//         console.error("Error generating response from API");
+//     }
+//   } catch (error) {
+//       console.error("Error:", error);
+//       api.sendMessage("An error occured while processing response", event.threadID, event.messageID);
+//   }
+// };
+
+// function formatFont(text) {
+//   return text;
+// }
+
+// module.exports.run = async function ({ api, event }) {};
 
 module.exports.config = {
   name: "bard",
@@ -315,81 +406,22 @@ module.exports.config = {
   cooldowns: 5,
 };
 
-async function convertImageToCaption(imageURL, api, event, inputText) {
-  try {
-    api.sendMessage("Generating response ✅", event.threadID, event.messageID);
+module.exports.run = async function({ api, event }) {
 
-    const response = await axios.get(`https://aki-gemini-pro-vision.vercel.app/gemini?prompt=${encodeURIComponent(inputText)}&url=${encodeURIComponent(imageURL)}`);
-    const caption = response.data.response;
-
-    if (caption) {
-      const formattedCaption = formatFont(caption);
-      api.sendMessage(`${formattedCaption}`, event.threadID, event.messageID);
-    } else {
-      api.sendMessage("Failed to recognize image.", event.threadID, event.messageID);
-    }
-  } catch (error) {
-    console.error("Error while recoginizing image:", error);
-    api.sendMessage("An error occured while recognizing the image.", event.threadID, event.messageID);
-  }
-}
-
-module.exports.handleEvent = async function ({ api, event }) {
-  if (!(event.body.toLowerCase().startsWith(`${global.config.PREFIX}ai`))) return;
-
-  const args = event.body.split(/\s+/);
-  args.shift();
-
-  if (event.type === "message_reply") {
-    if (event.messageReply.attachments[0]) {
-      const attachment = event.messageReply.attachments[0];
-
-       if (attachment.type === "photo") {
-        const imageURL = attachment.url;
-        convertImageToCaption(imageURL, api, event, args.join(' '));
-        return;
-      }
-    }
-  }
-
-  const inputText = args.join(' ');
-
-  if (!inputText && (!event.messageReply.attachments[0] || event.messageReply.attachments[0].type !== "photo")) {
-    return api.sendMessage("Hello, I'm GPT-4 by Ronnel. How may I help you?", event.threadID, event.messageID);
-  }
-
-  if (args[0] === "on") {
-    fontEnabled = true;
-    api.sendMessage({ body: "Font Formatting Enabled" }, event.threadID, event.messageID);
-    return;
-  }
-
-  if (args[0] === "off") {
-    fontEnabled = false;
-    api.sendMessage({ body: "Font Formatting Disabled" }, event.threadID, event.messageID);
-    return;
-  }
-
-  api.sendMessage("Generating response ✅", event.threadID, event.messageID);
-
-  try {
+try {
+    const axios = require('axios');
     var uid = event.senderID;
-    const response = await axios.get(`https://deku-rest-api.replit.app/gpt4?prompt=${encodeURIComponent(inputText)}&uid=${uid}`);
-    if (response.status === 200 && response.data.gpt4) {
-        let formattedResponse = formatFont(response.data.gpt4);
-        formattedResponse = formattedResponse.replace(/\n\[Image of .*?\]|(\*\*)/g, '').replace(/^\*/gm, '•');
-        api.sendMessage(`${formattedResponse}`, event.threadID, event.messageID);
-    } else {
-        console.error("Error generating response from API");
+    const query = args.join(' ');
+    if(!query){
+      return api.sendMessage(`Hello, I'm GPT-4 By Ronnel. How may I help you`, event.threadID, event.messageID);
     }
-  } catch (error) {
-      console.error("Error:", error);
-      api.sendMessage("An error occured while processing response", event.threadID, event.messageID);
-  }
-};
-
-function formatFont(text) {
-  return text;
+    const querylink = await axios.get(`https://deku-rest-api.replit.app/gpt4?prompt=${encodeURIComponent(query)}&uid=${uid}`);
+    api.sendMessage(`Generating response ✅`, event.threadID, event.messageID);
+    const result = querylink.data.gp4;
+    return api.sendMessage(result, event.threadID, event.messageID);
+} catch (error) {
+    return api.sendMessage(`Something went wrong`, event.threadID, event.messageID);  
 }
 
-module.exports.run = async function ({ api, event }) {};
+
+}
