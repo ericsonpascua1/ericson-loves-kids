6b398e3f7d0b533f341a27f4a5554da4a8182b27
ericson_pascua <167420722+Nathanielpradas14@users.noreply.github.com>
2024-05-19 21:46:21 +0800

    Update antiout.js

--- modules/events/antiout.js
+++ modules/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "Aki Hayakawa",
+ credits: "ericson終.",
  description: "Listen events"
 };
 
@@ -11,12 +11,12 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (data.antiout == false) return;
  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
- const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "na kick si kupal, buti nga";
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
     api.sendMessage(`Failed to re-add ${name}.`, event.threadID)
-   } else api.sendMessage(`Anti-out is active. Re-adding ${name} back`, event.threadID);
+   } else api.sendMessage(`tanga wala kang takas dito ${name} `, event.threadID);
   })
  }
                             }
