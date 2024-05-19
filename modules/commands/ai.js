b271d09c8cfec062169e9c780e4dfb225d618d54
ericson_pascua <167420722+Nathanielpradas14@users.noreply.github.com>
2024-05-20 02:25:52 +0800

    Update ai.js

--- modules/commands/ai.js
+++ modules/commands/ai.js
@@ -1,39 +1,44 @@
-const { Hercai } = require('hercai');
-const herc = new Hercai();
-
 module.exports.config = {
-  name: 'ai',
-  version: '1.1.0',
-  hasPermssion: 0,
-  credits: 'Yan Maglinte | Liane Cagara',
-  description: 'An AI command using Hercai API!',
-  usePrefix: false,
-  allowPrefix: true,
-  commandCategory: 'chatbots',
-  usages: 'Ai [prompt]',
-  cooldowns: 5,
+		name: "ai",
+		version: "1.0.0",
+		role: 0,
+		credits: "ericson",
+		hasPrefix: false,
+		description: "This module provides AI-powered responses using GPT-3.",
+		usage: "<question>",
+		cooldowns: 5,
+	usePrefix: false,
+	  hasPermission: 0,
+	  usages: "<question>",
+		cooldown: 5,
+	 commandCategory: "gpt4",
+		aliases: ["Gpt3"]
 };
 
-module.exports.run = async function ({ api, event, args, box }) {
-  const prompt = args.join(' ');
-  if (!box) {
-    return api.sendMessage(`Unsupported.`, event.threadID);
-  }
+module.exports.run = async function ({ api, event, args, _GLOBAL, client}) {
+		try {
+				const { G4F } = require("g4f");
 
-  try {
-    // Available Models: "v3", "v3-32k", "turbo", "turbo-16k", "gemini"
-    if (!prompt) {
-      box.reply('Please specify a message!');
-      box.react('❓');
-    } else {
-      const info = await box.reply(`Fetching answer...`);
-      box.react('⏱️');
-      const response = await herc.question({ model: 'v3', content: prompt });
-      await box.edit(response.reply, info.messageID);
-      box.react('');
-    }
-  } catch (error) {
-    box.reply('⚠️ Something went wrong: ' + error);
-    box.react('⚠️');
-  }
-};
+				function reply(a) {
+						api.sendMessage(a, event.threadID, event.messageID);
+				}
+
+				const g4f = new G4F();
+				const textInput = args.join(' ');
+				if (!textInput) return reply('Please provide a question.');
+
+				const messages = [
+						{ role: "user", content: textInput }
+				];
+				const options = {
+						provider: g4f.providers.GPT,
+						model: "gpt-3.5-turbo",
+						debug: true,
+						proxy: `${global.config.UPDATE}`
+				};
+				const response = await g4f.chatCompletion(messages, options);
+				reply(response);
+		} catch (e) {
+				return reply(e.message);
+		}
+}
