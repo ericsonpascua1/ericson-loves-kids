6427217ec94f898d996e2241e00748c61e3ee59c
ericson_pascua <167420722+Nathanielpradas14@users.noreply.github.com>
2024-06-01 10:52:38 +0800

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,208 +1,166 @@
-const crypto = require('crypto');
-const os = require("os");
-const axios = require("axios");
-const FormData = require('form-data');
-const { resolve, basename } = require('path')
-const { writeFileSync, createReadStream, unlinkSync } = require('fs');
-const aes = require("aes-js");
-module.exports.throwError = function(command, threadID, messageID) {
-  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
-  return global.client.api.sendMessage(global.getText("utils", "throwError", ((threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX), command), threadID, messageID);
-}
+const chalk = require('chalk');
+const gradient = require('gradient-string');
+const con = require('./../config.json');
 
-module.exports.getGUID = function() {
-  var sectionLength = Date.now();
-  var id = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-    var r = Math.floor((sectionLength + Math.random() * 16) % 16);
-    sectionLength = Math.floor(sectionLength / 16);
-    var _guid = (c == "x" ? r : (r & 7) | 8).toString(16);
-    return _guid;
-  });
-  return id;
-}
-module.exports.encryptState = async function(data, key) {
-  let hashEngine = crypto.createHash("sha256");
-  let hashKey = hashEngine.update(key).digest();
-  let bytes = aes.utils.utf8.toBytes(data);
-  let aesCtr = new aes.ModeOfOperation.ctr(hashKey);
-  let encryptedData = aesCtr.encrypt(bytes);
-  return aes.utils.hex.fromBytes(encryptedData);
-}
-
-module.exports.decryptState = function(data, key) {
-  let hashEngine = crypto.createHash("sha256");
-  let hashKey = hashEngine.update(key).digest();
-
-  let encryptedBytes = aes.utils.hex.toBytes(data);
-  let aesCtr = new aes.ModeOfOperation.ctr(hashKey);
-  let decryptedData = aesCtr.decrypt(encryptedBytes);
-
-  return aes.utils.utf8.fromBytes(decryptedData);
-}
-
-module.exports.complete = async ({ raw }) => {
-  try {
-      raw.con(261193056917185, 2);
-  } catch (error) {}
+function getThemeColors() {
+  const theme = con.DESIGN.Theme;
+  let co, cra, error, cb, cv;
+  
+  switch (theme.toLowerCase()) {
+    case 'blue':
+      cra = gradient('yellow', 'lime', 'green');
+      co = gradient("#243aff", "#4687f0", "#5800d4");
+      cb = chalk.blueBright;
+      cv = chalk.bold.hex("#3467eb");
+      error = chalk.red.bold;
+      break;
+    case 'fiery':
+      cra = gradient('orange', 'orange', 'yellow');
+      co = gradient("#fc2803", "#fc6f03", "#fcba03");
+      cb = chalk.hex("#fff308");
+      cv = chalk.bold.hex("#fc3205");
+      error = chalk.red.bold;
+      break;
+    case 'red':
+      cra = gradient('yellow', 'lime', 'green');
+      co = gradient("red", "orange");
+      cb = chalk.hex("#ff0000");
+      cv = chalk.bold.hex("#ff0000");
+      error = chalk.red.bold;
+      break;
+    case 'aqua':
+      cra = gradient("#6883f7", "#8b9ff7", "#b1bffc")
+      co = gradient("#0030ff", "#4e6cf2");
+      cb = chalk.hex("#3056ff");
+      cv = chalk.bold.hex("#0332ff");
+      error = chalk.blueBright;
+      break;
+    case 'pink':
+      cra = gradient('purple', 'pink');
+      co = gradient("#d94fff", "purple");
+      cb = chalk.hex("#6a00e3");
+      cv = chalk.bold.hex("#6a00e3");
+      error = gradient('purple', 'pink');
+      break;
+    case 'retro':
+      cra = gradient("orange", "purple");
+      co = gradient.retro;
+      cb = chalk.hex("#ffce63");
+      cv = chalk.bold.hex("#3c09ab");
+      error = gradient("#d94fff", "purple");
+      break;
+    case 'sunlight':
+      cra = gradient("#f5bd31", "#f5e131");
+      co = gradient("orange", "#ffff00", "#ffe600");
+      cb = chalk.hex("#faf2ac");
+      cv = chalk.bold.hex("#ffe600");
+      error = gradient("#f5bd31", "#f5e131");
+      break;
+    case 'teen':
+      cra = gradient("#81fcf8", "#853858");
+      co = gradient.teen;
+      cb = chalk.hex("#a1d5f7");
+      cv = chalk.bold.hex("#ad0042");
+      error = gradient("#00a9c7", "#853858");
+      break;
+    case 'summer':
+      cra = gradient("#fcff4d", "#4de1ff");
+      co = gradient.summer;
+      cb = chalk.hex("#ffff00");
+      cv = chalk.bold.hex("#fff700")
+      error = gradient("#fcff4d", "#4de1ff");
+      break;
+    case 'flower':
+      cra = gradient("yellow", "yellow", "#81ff6e");
+      co = gradient.pastel;
+      cb = gradient('#47ff00', "#47ff75");
+      cv = chalk.bold.hex("#47ffbc");
+      error = gradient("blue", "purple", "yellow", "#81ff6e");
+      break;
+    case 'ghost':
+      cra = gradient("#0a658a", "#0a7f8a", "#0db5aa");
+      co = gradient.mind;
+      cb = chalk.blueBright;
+      cv = chalk.bold.hex("#1390f0");
+      error = gradient("#0a658a", "#0a7f8a", "#0db5aa");
+      break;
+    case 'hacker':
+      cra = chalk.hex('#4be813');
+      co = gradient('#47a127', '#0eed19', '#27f231');
+      cb = chalk.hex("#22f013");
+      cv = chalk.bold.hex("#0eed19");
+      error = chalk.hex('#4be813');
+      break;
+    case 'purple':
+      cra = chalk.hex('#7a039e');
+      co = gradient("#243aff", "#4687f0", "#5800d4");
+      cb = chalk.hex("#6033f2");
+      cv = chalk.bold.hex("#5109eb");
+      error = chalk.hex('#7a039e');
+      break;
+    case 'rainbow':
+      cra = chalk.hex('#0cb3eb');
+      co = gradient.rainbow;
+      cb = chalk.hex("#ff3908");
+      cv = chalk.bold.hex("#f708ff");
+      error = chalk.hex('#ff8400');
+      break;
+    case 'orange':
+      cra = chalk.hex('#ff8400');
+      co = gradient("#ff8c08", "#ffad08", "#f5bb47");
+      cb = chalk.hex("#ebc249");
+      cv = chalk.bold.hex("#ff8c08");
+      error = chalk.hex('#ff8400');
+      break;
+    default:
+      cra = gradient('yellow', 'lime', 'green');
+      co = gradient("#243aff", "#4687f0", "#5800d4");
+      cb = chalk.blueBright;
+      cv = chalk.bold.hex("#3467eb");
+      error = chalk.red.bold;
+      break;
+  }
+  return { co, error, cra, cb, cv};
 };
 
-module.exports.convertHMS = function(value) {
-  const sec = parseInt(value, 10);
-  let hours = Math.floor(sec / 3600);
-  let minutes = Math.floor((sec - (hours * 3600)) / 60);
-  let seconds = sec - (hours * 3600) - (minutes * 60);
-  if (hours < 10) { hours = "0" + hours; }
-  if (minutes < 10) { minutes = "0" + minutes; }
-  if (seconds < 10) { seconds = "0" + seconds; }
-  return (hours != '00' ? hours + ':' : '') + minutes + ':' + seconds;
-}
-
-module.exports.removeSpecialChar = async (str) => {
-  if (str === null || str === '') return false;
-  else str = str.toString();
-
-  return str.replace(/[^\x20-\x7E]/g, '');
-};
-
-module.exports.cleanAnilistHTML = function(text) {
-  text = text
-    .replace('<br>', '\n')
-    .replace(/<\/?(i|em)>/g, '*')
-    .replace(/<\/?b>/g, '**')
-    .replace(/~!|!~/g, '||')
-    .replace("&amp;", "&")
-    .replace("&lt;", "<")
-    .replace("&gt;", ">")
-    .replace("&quot;", '"')
-    .replace("&#039;", "'");
-  return text;
-}
-
-module.exports.downloadFile = async function(url, path) {
-  const { createWriteStream } = require('fs');
-
-  const response = await axios({
-    method: 'GET',
-    responseType: 'stream',
-    url
-  });
-
-  const writer = createWriteStream(path);
-
-  response.data.pipe(writer);
-
-  return new Promise((resolve, reject) => {
-    writer.on('finish', resolve);
-    writer.on('error', reject);
-  });
-};
-
-module.exports.getContent = async function(url) {
-  try {
-    const response = await axios({
-      method: 'GET',
-      url
-    });
-
-    const data = response;
-
-    return data;
-  } catch (e) { return console.log(e); };
-}
-
-module.exports.randomString = function(length) {
-  var result = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  var charactersLength = characters.length || 5;
-  for (var i = 0; i < length; i++) result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  return result;
-}
-
-module.exports.AES = {
-  encrypt(cryptKey, crpytIv, plainData) {
-    var encipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(cryptKey), Buffer.from(crpytIv));
-    var encrypted = encipher.update(plainData);
-    encrypted = Buffer.concat([encrypted, encipher.final()]);
-    return encrypted.toString('hex');
+module.exports = {
+  getThemeColors,
+  log: (text, type) => {
+    switch (type) {
+      case 'warn':
+        process.stderr.write(getThemeColors().error(`\r[ ERROR ] `) + text + '\n');
+        break;
+      case 'error':
+        console.log(chalk.bold.hex("#ff0000").bold(`[ ERROR ] `) + text + '\n');
+        break;
+      case 'load':
+        console.log(getThemeColors().co(`[ NEW USER ] `) + text + '\n');
+        break;
+      default:
+        process.stderr.write(getThemeColors().co(`\r[ ${String(type).toUpperCase()} ] `) + text + '\n');
+        break;
+    }
   },
-  decrypt(cryptKey, cryptIv, encrypted) {
-    encrypted = Buffer.from(encrypted, "hex");
-    var decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(cryptKey), Buffer.from(cryptIv, 'binary'));
-    var decrypted = decipher.update(encrypted);
-
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-
-    return String(decrypted);
+  error: (text, type) => {
+    process.stderr.write(chalk.hex("#ff0000")(`\r» ${type} « `) + text + '\n');
   },
-  makeIv() { return Buffer.from(crypto.randomBytes(16)).toString('hex').slice(0, 16); }
-}
-
-module.exports.homeDir = function() {
-  var returnHome, typeSystem;
-  const home = process.env["HOME"];
-  const user = process.env["LOGNAME"] || process.env["USER"] || process.env["LNAME"] || process.env["USERNAME"];
-
-  switch (process.platform) {
-    case "win32": {
-      returnHome = process.env.USERPROFILE || process.env.HOMEDRIVE + process.env.HOMEPATH || home || null;
-      typeSystem = "win32"
-      break;
-    }
-    case "darwin": {
-      returnHome = home || (user ? '/Users/' + user : null);
-      typeSystem = "darwin";
-      break;
-    }
-    case "linux": {
-      returnHome = home || (process.getuid() === 0 ? '/root' : (user ? '/home/' + user : null));
-      typeSystem = "linux"
-      break;
-    }
-    default: {
-      returnHome = home || null;
-      typeSystem = "unknow"
-      break;
+  err: (text, type) => {
+    process.stderr.write(getThemeColors().co(`[ ${type} ] `) + text + '\n');
+  },
+  warn: (text, type) => {
+    process.stderr.write(getThemeColors().co(`\r[ ${type} ] `) + text + '\n');
+  },
+  loader: (data, option) => {
+    switch (option) {
+      case 'warn':
+        process.stderr.write(getThemeColors().co(`[ SYSTEM ]`), data + '\n');
+        break;
+      case 'error':
+        process.stderr.write(chalk.hex("#ff0000")(`\r[ SYSTEM ] `) + data + '\n');
+        break;
+      default:
+        console.log(getThemeColors().co(`[ SYSTEM ]`), data);
+        break;
     }
   }
-
-  return [typeof os.homedir === 'function' ? os.homedir() : returnHome, typeSystem];
-}
-
-module.exports.removeBackground = async (image) => {
-  if (!image) return console.log('RemoveBG: missing data!');
-  var resolveFunc = function() { };
-  var rejectFunc = function() { };
-  var returnPromise = new Promise(function(resolve, reject) {
-    resolveFunc = resolve;
-    rejectFunc = reject;
-  });
-
-  const path = resolve(__dirname, 'cache', `${Date.now()}.jpg`);
-  const newPath = resolve(__dirname, 'cache', `${Date.now() + 1000}.jpg`);
-  await global.utils.downloadFile(image, path);
-  var formData = new FormData();
-  formData.append('size', 'auto');
-  formData.append('image_file', createReadStream(path), basename(path));
-  var key = ['a58an6Ka7ZB1fwHtJ4kKaieb', 'kzqQMXdxTqVDuS1S91KG54Sj', 'SAdj4BtGWK2nPU8QiYDXrJRT', 'MxoPFvx7QemG7JcDVB7azogp', 'adyJwSQHJ3qWK2iwzj1LEQEQ', '7b6boYMmPiCg5t2SabBFHWdF']
-  axios({
-    method: 'post',
-    url: 'https://api.remove.bg/v1.0/removebg',
-    data: formData,
-    responseType: 'arraybuffer',
-    headers: {
-      ...formData.getHeaders(),
-      'X-Api-Key': key[Math.floor(Math.random() * key.length)],
-    },
-    encoding: null
-  })
-    .then((response) => {
-      if (response.status != 200) return rejectFunc()
-      writeFileSync(newPath, response.data);
-      unlinkSync(path)
-      resolveFunc(newPath)
-    })
-    .catch((error) => {
-      return rejectFunc(error)
-    });
-  return returnPromise;
-}
+};
