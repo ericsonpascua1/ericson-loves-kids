e3cf885f496bd4c99ab5949f4634d227e1a91515
ericson_pascua <167420722+Nathanielpradas14@users.noreply.github.com>
2024-06-04 22:19:40 +0800

    Update join.js

--- modules/commands/join.js
+++ modules/commands/join.js
@@ -2,45 +2,43 @@ const chalk = require('chalk');
 module.exports.config = {
     name: "join",
     version: "1.0.1",
-    hasPermssion: 2,
-    credits: "ericson",//nakikifork ka na nga lang papalitan mopa ung credits tukmol ka 
-    description: "Join the Bot boxes are in",
-  usePrefix: true,
-    commandCategory: "System",
+    permission: 2,
+    credits: "ericson",
+    prefix: true,
+    description: "join the bot groups are in",
+    category: "admin",
+    premium: false,
     usages: "",
     cooldowns: 5
 };
- module.exports.onLoad = () => {
-  console.log(chalk.bold.hex("#00c300").bold("============ SUCCESFULLY LOADED THE JOIN COMMAND ============"));
-  }
 module.exports.handleReply = async function({ api, event, handleReply, Threads }) {
   var { threadID, messageID, senderID, body } = event;
   var { ID } = handleReply;
   console.log(ID)
-  if (!body || !parseInt(body)) return api.sendMessage('Your selection must be a number.', threadID, messageID);
-  if ((parseInt(body) - 1) > ID.length) return api.sendMessage("Your pick is not on the list", threadID, messageID);
+  if (!body || !parseInt(body)) return api.sendMessage('your selection must be a number.', threadID, messageID);
+  if ((parseInt(body) - 1) > ID.length) return api.sendMessage("your pick is not on the list", threadID, messageID);
   try {
     var threadInfo = await Threads.getInfo(ID[body - 1]);
     var { participantIDs, approvalMode, adminIDs } = threadInfo;
-    if (participantIDs.includes(senderID)) return api.sendMessage(`You are already in this group.`, threadID, messageID);
+    if (participantIDs.includes(senderID)) return api.sendMessage(`you are already in this group.`, threadID, messageID);
     api.addUserToGroup(senderID, ID[body - 1]);
-    if (approvalMode == true && !adminIDs.some(item => item.id) == api.getCurrentUserID()) return api.sendMessage("Added you to the group's approval list...Custom yourself.", threadID, messageID);
-    else return api.sendMessage(`My wife just added you to the group ${threadInfo.threadName} already. Check in the waiting or spam message section if you don't see the box \nA lot 💟`, threadID, messageID);
+    if (approvalMode == true && !adminIDs.some(item => item.id) == api.getCurrentUserID()) return api.sendMessage("added you to the group's approval list custom yourself.", threadID, messageID);
+    else return api.sendMessage(`you have joined to ${threadInfo.threadName}. check in the message request or spam message, if the group didn't exist in your message box maybe they've turned on the admin approval.`, threadID, messageID);
   } catch (error) {
-    return api.sendMessage(`I'm wrong so I can't add you to that group:<.\n\n${error}`, threadID, messageID);
+    return api.sendMessage(`i can't add you to that group\nerror : ${error}`, threadID, messageID);
   }
 }
 
 module.exports.run = async function({ api, event, Threads }) {
   var { threadID, messageID, senderID } = event;
-  var msg = `🔰==[ BOX LIST ]==🔰\n\n`, number = 0, ID = [];
+  var msg = `all groups\n\n`, number = 0, ID = [];
   var allThreads = await Threads.getAll();
   for (var i of allThreads) {
     number++;
     msg += `${number}. ${i.threadInfo.threadName}\n`;
     ID.push(i.threadID)
   }
-  msg += `\n👉 Reply this message with the number corresponding to the group you want to enter`
+  msg += `\nreply to this message with number if you want to join from that group`
   return api.sendMessage(msg, threadID, (error, info) => {
     global.client.handleReply.push({
       name: this.config.name,
