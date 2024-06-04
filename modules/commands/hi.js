ca75cf7a58c03c5e94181ff0bce70145061e275f
ericson_pascua <167420722+Nathanielpradas14@users.noreply.github.com>
2024-06-04 22:56:58 +0800

    Update hi.js

--- modules/commands/hi.js
+++ modules/commands/hi.js
@@ -1,93 +1,119 @@
-async function getUserName(api, senderID) {
-	try {
-		const userInfo = await api.getUserInfo(senderID);
-		return userInfo[senderID]?.name || "User";
-	} catch (error) {
-		console.log(error);
-		return "User";
+module.exports.config = {
+  name: "hi",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "ericson終.",
+  usePrefix: true,
+  description: "hi with sticker",
+  commandCategory: "QTV BOX",
+  usages: "[text]",
+  cooldowns: 5
+}
+
+module.exports.handleEvent = async ({ event, api, Users }) => {
+  let KEY = [ 
+    "hello",
+    "hi",
+    "hello po",
+    "hi po",
+    "hiii",
+    "helloo",
+    "loe",
+    "low",
+    "lo",
+    "hey",
+    "heyy",
+    "loe po",
+    "low po",
+    "hai",
+    "chào",
+    "chao",
+    "hí",
+    "híí",
+    "hì",
+    "hìì",
+    "lô",
+    "helo",
+    "hê nhô",
+    "goodevening",
+    "good evening",
+    "goodeve",
+    "gn",
+    "eve",
+    "evening",
+    "good afternoon",
+    "good aftie",
+    "aftie",
+    "afternoon"
+  ];
+  let thread = global.data.threadData.get(event.threadID) || {};
+  if (typeof thread["hi"] == "undefined", thread["hi"] == false) return
+  else {
+  if (KEY.includes(event.body) !== false) {
+    let data = ["422812141688367", "1775288509378520", "476426593020937", "476420733021523", "147663618749235", "466041158097347", "1528732074026137", "147663618749235", "476426753020921", "529233794205649", "1330360453820546"];
+    let sticker = data[Math.floor(Math.random() * data.length)];
+let juswa = ["have you eaten?", "what are you doing?", "how are you baby?", "I'm a chat bot nice to meet you", "I'm updating my commands, what are you doing?", "Can you interact with me using sim command?","You're so beautiful/handsome binibini/ginoo", "I love you mwa */kiss your forehead.","are you bored? talk to my admin(ericson終.)", "how are you my dear", "eat some sweets", "are you ok?", "be safe"];
+ let juswa1 = juswa[Math.floor(Math.random() * juswa.length)];
+    let moment = require("moment-timezone");
+    let hours = moment.tz('Asia/Manila').format('HHmm');
+    let session = (
+    hours > 0001 && hours <= 400 ? "bright morning" : 
+    hours > 401 && hours <= 700 ? "morning" :
+    hours > 701 && hours <= 1000 ? "morning" :
+    hours > 1001 && hours <= 1100 ? "morning" : 
+    hours > 1100 && hours <= 1500 ? "afternoon" : 
+    hours > 1501 && hours <= 1800 ? "evening" : 
+    hours > 1801 && hours <= 2100 ? "evening" : 
+    hours > 2101 && hours <= 2400 ? "late night and advance sleepwell" : 
+    "error");
+    var thu =
+moment.tz('Asia/Manila').format('dddd');
+  if (thu == 'Sunday') thu = 'sunday'
+  if (thu == 'Monday') thu = 'monday'
+  if (thu == 'Tuesday') thu = 'tuesday'
+  if (thu == 'Wednesday') thu = 'wednesday'
+  if (thu == "Thursday") thu = 'thursday'
+  if (thu == 'Friday') thu = 'friday'
+  if (thu == 'Saturday') thu = 'saturday'
+    let name = await Users.getNameUser(event.senderID);
+    let mentions = [];
+    mentions.push({
+      tag: name,
+      id: event.senderID
+    })
+    var message = [`Hi ${name}, have a good ${thu} baby, ${juswa1}`];
+    let message2 = message[Math.floor(Math.random() * message.length)];
+    let msg = {body: message2, mentions}
+    api.sendMessage(msg, event.threadID, (e, info) => {
+      setTimeout(() => {
+        api.sendMessage({sticker: sticker}, event.threadID);
+      }, 100)
+    }, event.messageID)
+  }
+  }
+}
+
+module.exports.languages = {
+  "vi": {
+    "on": "Bật",
+    "off": "Tắt",
+		"successText": `${this.config.name} thành công`,
+	},
+	"en": {
+		"on": "on",
+		"off": "off",
+		"successText": `${this.config.name} success!`,
 	}
 }
 
-let hiFlag = true;
-
-const config = {
-		name: "hi",
-		version: "2.0.0",
-		hasPermission: 0,
-		credits: "ericson",
-		description: "hi with sticker",
-	commandCategory: "no prefix",
-		usePrefix: true,
-		aliases: [],
-		usages: "",
-		cooldown: 1
-};
-
-const handleEvent = async ({ event, prefix, api }) => {
-		let KEY = [
-				"hello",
-				"hi",
-				"hello po",
-			   "low",
-			    "loe"
-		];
-
-		if (!hiFlag || !checkKeyword(event.body, KEY)) return;
-
-		let data = [
-				"184002922217363", "184023658881956", "184003212217334", "184002655550723", "184003438883978", "2379545595403511", "1926234657415528", "4046655705381617", "4046877352026119", "4046884992025355", "4070816262965561"
-		];
-		let sticker = data[Math.floor(Math.random() * data.length)];
-		let juswa = ["Have you eaten?", "What are you doing?", "How are you senpai?", "I'm a chat bot nice to meet you", "Do you need any help?", "Is there anything on your mind?",
-				"Tell me about your day",
-				"Have you watched any good movies lately?",
-				"What's your favorite hobby?",
-				"Do you believe in aliens?", "I'm updating my commands, What are you doing?", "Can you interact with my admin (ericson)", "You're so beautiful/handsome binibini/ginoo", "I love you mwa */kiss your forehead.", "Are you bored? talk to my admin", "How are you my dear", "Sana okay kalang.", "Are you ok?", "Be safe, Mwaa.", "Wag magpapagutom mahal.", `Use ${prefix}help to see my commands. `];
-		let juswa1 = juswa[Math.floor(Math.random() * juswa.length)];
-
-		let moment = require("moment-timezone");
-		let hours = moment.tz('Asia/Manila').format('HHmm');
-		let session = (
-				hours > 0001 && hours <= 400 ? "Blessed Morning" :
-				hours > 401 && hours <= 700 ? "Morning" :
-				hours > 701 && hours <= 1000 ? "Morning" :
-				hours > 1001 && hours <= 1100 ? "Morning" :
-				hours > 1100 && hours <= 1500 ? "Afternoon" :
-				hours > 1501 && hours <= 1800 ? "Evening " :
-				hours > 1801 && hours <= 2100 ? "Evening.." :
-				hours > 2101 && hours <= 2400 ? "Late Night Sleep Well..." :
-				"error");
-		let name = await getUserName(api, event.senderID);
-		let mentions = [];
-		mentions.push({
-				tag: name,
-				id: event.senderID
-		});
-		let msg = { body: `Hi ${name}, have a Good ${session} Master🥰, ${juswa1}`, mentions };
-		api.sendMessage(msg, event.threadID, (e, info) => {
-				setTimeout(() => {
-						api.sendMessage({ sticker: sticker }, event.threadID);
-				}, 100);
-		}, event.messageID);
-};
-
-const run = async ({ event, api }) => {
-		hiFlag = !hiFlag; 
-		return api.sendMessage(`${hiFlag ? "on" : "off"} success!`, event.threadID, event.messageID);
-};
-
-function checkKeyword(body, keywords) {
-		if (!body || !keywords) return false;
-		for (let keyword of keywords) {
-				if (body.includes(keyword)) {
-						return true;
-				}
-		}
-		return false;
+module.exports.run = async ({ event, api, Threads, getText }) => {
+  let { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+	if (typeof data["hi"] == "undefined" || data["hi"] == true) data["hi"] = false;
+	else data["hi"] = true;
+	await Threads.setData(threadID, {
+		data
+	});
+	global.data.threadData.set(threadID, data);
+	return api.sendMessage(`${(data["hi"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
 }
-
-module.exports = {
-		config,
-		handleEvent,
-		run
-};
