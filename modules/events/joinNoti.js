39ae8f788d394782358e9371947f5558168c4b04
ericsonpascua1 <ericsonpascua81@gmail.com>
2024-06-18 22:22:30 +0800

    Update joinNoti.js

--- modules/events/joinNoti.js
+++ modules/events/joinNoti.js
@@ -2,25 +2,31 @@ module.exports.config = {
   name: "join",
   eventType: ['log:subscribe'],
   version: "1.0.0",
-  credits: "Aki Hayakawa",
+  credits: "Mirai-Team", // FIXED BY YAN MAGLINTE
   description: "GROUP UPDATE NOTIFICATION"
 };
+
+const ADMIN = 'YOUR_NAME';
+const FB_LINK = 'YOUR_FACEBOOK_LINK';
+
 const fs = require('fs-extra');
+const { loadImage, createCanvas, registerFont } = require("canvas");
 const request = require('request');
+//const { join } = require('path');
 const axios = require('axios');
-const jimp = require("jimp");
+const jimp = require("jimp")
 const fontlink = 'https://drive.google.com/u/0/uc?id=1ZwFqYB-x6S9MjPfYm3t3SP1joohGl4iw&export=download'
 let PRFX = `${global.config.PREFIX}`;
 
 module.exports.circle = async (image) => {
   image = await jimp.read(image);
   image.circle();
-  return await image.getBufferAsync("image/gif");
+  return await image.getBufferAsync("image/png");
 }
 
 let suffix;
 
-module.exports.run = async function ({ api, event, Users }) {
+module.exports.run = async function({ api, event, Users }) {
   var fullYear = global.client.getTime("fullYear");
   var getHours = await global.client.getTime("hours");
   var session = `${getHours < 3 ? "midnight" : getHours < 8 ? "Early morning" : getHours < 12 ? "noon" : getHours < 17 ? "afternoon" : getHours < 23 ? "evening" : "midnight"}`
@@ -43,8 +49,19 @@ module.exports.run = async function ({ api, event, Users }) {
     return;
   }
   if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-  //   api.changeNickname(`ᴀᴋi`, threadID, api.getCurrentUserID());
-    return api.sendMessage("", event.threadID, () => api.sendMessage({ body: `Connected successfully!\nThank you for using my bot. Have fun!`}, threadID));
+    //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+    
+    let gifUrl = 'https://i.imgur.com/4HMupHz.gif';
+let gifPath = __dirname + '/cache/join/join.gif';
+
+axios.get(gifUrl, { responseType: 'arraybuffer' })
+.then(response => {
+    fs.writeFileSync(gifPath, response.data);
+    return api.sendMessage("Hey There!", event.threadID, () => api.sendMessage({ body: `✅ Group Connection in ${threadName} at ${session} success! \n\n➭ Current Commands: ${commands.size}\n➭ Bot Prefix: ${global.config.PREFIX}\n➭ Version: ${global.config.version}\n➭ Admin: ‹${ADMIN}›\n➭ Facebook: ‹${FB_LINK}›\n➭ Use ${PRFX}help to view command details\n➭ Added bot at: ⟨ ${time} ⟩〈 ${thu} 〉`, attachment: fs.createReadStream(gifPath)}, threadID));
+})
+.catch(error => {
+    console.error(error);
+});
   }
   else {
     try {
@@ -67,36 +84,39 @@ module.exports.run = async function ({ api, event, Users }) {
       // console.log(event.logMessageData.addedParticipants)
       var id = [];
       for (let o = 0; o < event.logMessageData.addedParticipants.length; o++) {
-        let pathImg = __dirname + `/cache/join/${o}.gif`;
-        // let pathAva = __dirname + `/cache/join/avt.png`;
-        // let avtAnime = (await axios.get(encodeURI(
-        //   `https://graph.facebook.com/${event.logMessageData.addedParticipants[o].userFbId}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`), { responseType: "arraybuffer" })).data;
+        let pathImg = __dirname + `/cache/join/${o}.png`;
+        let pathAva = __dirname + `/cache/join/avt.png`;
+        let avtAnime = (await axios.get(encodeURI(
+          `https://graph.facebook.com/${event.logMessageData.addedParticipants[o].userFbId}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`), { responseType: "arraybuffer" })).data;
         var ok = [
-          'https://i.imgur.com/gz7epfD.gif'
+          'https://i.imgur.com/dDSh0wc.jpeg',
+          'https://i.imgur.com/UucSRWJ.jpeg',
+          'https://i.imgur.com/OYzHKNE.jpeg',
+          'https://i.imgur.com/V5L9dPi.jpeg',
+          'https://i.imgur.com/M7HEAMA.jpeg'
         ]
         let background = (await axios.get(encodeURI(`${ok[Math.floor(Math.random() * ok.length)]}`), { responseType: "arraybuffer", })).data;
-        // fs.writeFileSync(pathAva, Buffer.from(avtAnime, "utf-8"));
+        fs.writeFileSync(pathAva, Buffer.from(avtAnime, "utf-8"));
         fs.writeFileSync(pathImg, Buffer.from(background, "utf-8"));
-        // var avatar = await this.circle(pathAva);
-        // let baseImage = await loadImage(pathImg);
-        // let baseAva = await loadImage(avatar);
-        // registerFont(__dirname + `/cache/font/Semi.ttf`, {
-        //   family: "Semi"
-        // });
-        // let canvas = createCanvas(1902, 1082);
-        // console.log(canvas.width, canvas.height)
-        // let ctx = canvas.getContext("2d");
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-        // ctx.drawImage(baseAva, canvas.width / 2 - 188, canvas.height / 2 - 375, 375, 355);
-        // ctx.fillStyle = "#FFF";
-        // ctx.textAlign = "center";
-        // ctx.font = `155px Semi`;
-        // ctx.fillText(`${event.logMessageData.addedParticipants[o].fullName}`, canvas.width / 2 + 20, canvas.height / 2 + 100);
-        // ctx.save();
-        // ctx.font = `75px Semi`;
-        // ctx.fillText(`Welcome to ${threadName}`, canvas.width / 2 - 15, canvas.height / 2 + 235)
-
+        var avatar = await this.circle(pathAva);
+        let baseImage = await loadImage(pathImg);
+        let baseAva = await loadImage(avatar);
+        registerFont(__dirname + `/cache/font/Semi.ttf`, {
+          family: "Semi"
+        });
+        let canvas = createCanvas(1902, 1082);
+        console.log(canvas.width, canvas.height)
+        let ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(baseAva, canvas.width / 2 - 188, canvas.height / 2 - 375, 375, 355);
+        ctx.fillStyle = "#FFF";
+        ctx.textAlign = "center";
+        ctx.font = `155px Semi`;
+        ctx.fillText(`${event.logMessageData.addedParticipants[o].fullName}`, canvas.width / 2 + 20, canvas.height / 2 + 100);
+        ctx.save();
+        ctx.font = `75px Semi`;
+        ctx.fillText(`Welcome to ${threadName}`, canvas.width / 2 - 15, canvas.height / 2 + 235)
         const number = participantIDs.length - o;
 
         if (number === 11 || number === 12 || number === 13) {
@@ -119,14 +139,14 @@ module.exports.run = async function ({ api, event, Users }) {
           }
         }
 
-        // ctx.fillText(`You are the ${number}${suffix} member of this group`, canvas.width / 2 - 15, canvas.height / 2 + 350);
-        // ctx.restore();
-        // const imageBuffer = canvas.toBuffer();
-        // fs.writeFileSync(pathImg, imageBuffer);
-        abx.push(fs.createReadStream(__dirname + `/cache/join/${o}.gif`))
+        ctx.fillText(`You are the ${number}${suffix} member of this group`, canvas.width / 2 - 15, canvas.height / 2 + 350);
+        ctx.restore();
+        const imageBuffer = canvas.toBuffer();
+        fs.writeFileSync(pathImg, imageBuffer);
+        abx.push(fs.createReadStream(__dirname + `/cache/join/${o}.png`))
       }
       memLength.sort((a, b) => a - b);
-      (typeof threadData.customJoin == "undefined") ? msg = `Welcome {name} to the group {threadName}. {type} are the group's {soThanhVien}${suffix} member. Enjoy!` : msg = threadData.customJoin;
+      (typeof threadData.customJoin == "undefined") ? msg = `🌟 Welcome new member {name} to the group {threadName}\n→ URL Profile:\nhttps://www.facebook.com/profile.php?id={iduser}\n→ {type} are the group's {soThanhVien}${suffix} member\n→ Added to the group by: {author}\n→ Added by facebook link: https://www.facebook.com/profile.php?id={uidAuthor}\n─────────────────\n[ {time} - {thu} ]` : msg = threadData.customJoin;
       var nameAuthor = await Users.getNameUser(event.author)
       msg = msg
         .replace(/\{iduser}/g, iduser.join(', '))
@@ -143,7 +163,7 @@ module.exports.run = async function ({ api, event, Users }) {
       var formPush = { body: msg, attachment: abx, mentions }
       api.sendMessage(formPush, threadID);
       for (let ii = 0; ii < parseInt(id.length); ii++) {
-        fs.unlinkSync(__dirname + `/cache/join/${ii}.gif`)
+        fs.unlinkSync(__dirname + `/cache/join/${ii}.png`)
       }
     } catch (e) { return console.log(e) };
   }
